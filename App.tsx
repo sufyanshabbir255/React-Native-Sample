0625d99772310e13b0d21c0d7af4d49ff75b7aef
sufyanshabbir255 <sufyanshabbir255@gmail.com>
2023-10-02 13:42:52 +0500

    initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Image,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <SafeAreaView>
+    
+    </SafeAreaView>
+  );
+}
+
+export default App;
